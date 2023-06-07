93c83f1ad5dbb9c589e5d4429ddbd8eae7a4d5a4
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-06-07 14:13:54 -0400

    made this a little prettier

--- Program.cs
+++ Program.cs
@@ -5,45 +5,24 @@ namespace Garage
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            Zero fxs = new Zero();
-            Zero fxe = new Zero();
-            Tesla modelS = new Tesla();
-            Cessna mx410 = new Cessna();
-            Ram ram1500 = new Ram();
+            var vehicles = new List<Vehicle>
+            {
+                new Zero { MainColor = "Midnight Blue" },
+                new Zero { MainColor = "Black" },
+                new Tesla { MainColor = "Burgundy" },
+                new Cessna { MainColor = "White" },
+                new Ram { MainColor = "Silver" }
+            };
 
-            fxs.MainColor = "Midnight Blue";
-            fxe.MainColor = "Black";
-            modelS.MainColor = "Burgundy";
-            mx410.MainColor = "White";
-            ram1500.MainColor = "Silver";
-
-            fxs.Drive();
-            fxs.Turn("right");
-            fxs.Stop();
-            Console.WriteLine();
-
-            fxe.Drive();
-            fxe.Turn("right");
-            fxe.Stop();
-            Console.WriteLine();
-
-            modelS.Drive();
-            modelS.Turn("right");
-            modelS.Stop();
-            Console.WriteLine();
-
-            ram1500.Drive();
-            ram1500.Turn("right");
-            ram1500.Stop();
-            Console.WriteLine();  
-
-            mx410.Drive();
-            mx410.Turn("right");
-            mx410.Stop();
-            Console.WriteLine();
-      
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Drive();
+                vehicle.Turn("right");
+                vehicle.Stop();
+                Console.WriteLine();
+            }
         }
     }
 }
